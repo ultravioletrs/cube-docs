333f34bbc8c4d0936788f6736e7a4faac58acb36
ianmuchyri <ianmuchiri8@gmail.com>
2026-06-05 19:18:28 +0300

    fix search functionality
    Signed-off-by: ianmuchyri <ianmuchiri8@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import { DocsLayout } from "fumadocs-ui/layouts/docs";
-import { RootProvider } from "fumadocs-ui/provider/next";
 import type { Metadata } from "next";
 import type { ReactNode } from "react";
+import { Provider } from "@/components/provider";
 import { baseOptions } from "@/lib/layout.shared";
 import { baseUrl, createMetadata } from "@/lib/metadata";
 import { source } from "@/lib/source";
@@ -22,11 +22,11 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="flex flex-col min-h-screen bg-background text-foreground font-body">
-        <RootProvider>
+        <Provider>
           <DocsLayout tree={source.getPageTree()} {...baseOptions()}>
             {children}
           </DocsLayout>
-        </RootProvider>
+        </Provider>
       </body>
     </html>
   );
